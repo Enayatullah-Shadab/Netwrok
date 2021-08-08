e7e4e6fc786991fe1d1ec5c317c1d74d079f81a8
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-08 01:18:30 -0600

    Thread&profile Components created

---
+++ src/components/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="profile">
+    <!-- this is my profile -->
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Profile',
+  setup() {
+    return {
+      // my methods will be applied here
+    }
+  }
+}
+</script>
---
+++ src/components/Thread.vue
@@ -0,0 +1,15 @@
+<template>
+  this will be called in postCard
+</template>
+<script>
+
+export default {
+  name: 'Post',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
